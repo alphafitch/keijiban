e950a7eb1cfaa1f831e9b4f7121a5d320ddafb31
Christopher Fisher <chris@alphafitch.uk>
2015-12-25 16:08:20 +0000

    Initial work for #12 to convert the info page into a sidenav

--- app/common/infoButton/infoButtonController.js
+++ app/common/infoButton/infoButtonController.js
@@ -1,7 +1,72 @@
-keijibanApp.controller('infoButtonController', function($scope, $location) {
+keijibanApp.controller('infoButtonController', function($scope, $location,  $timeout, $mdSidenav, $log) {
 
     $scope.goToInfo = function() {
         $location.url("info");
+    };
+
+
+    $scope.toggleLeft = buildToggler('left');
+
+    $scope.isOpenLeft = function(){
+      return $mdSidenav('left').isOpen();
+    };
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+
+    }).controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+
+    $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+
+    }).controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+
+    $scope.close = function () {
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
     };
 
 });
