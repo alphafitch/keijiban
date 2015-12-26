ce78630aa7afe2d8fdef8ab3bcdbdccbb3b41451
Christopher Fisher <chris@alphafitch.uk>
2015-12-26 12:09:06 +0000

    Removing debounce function from the menu controllers which isn't needed

--- app/common/editMenu/editMenuController.js
+++ app/common/editMenu/editMenuController.js
@@ -10,21 +10,6 @@ keijibanApp.controller('editMenuController', function($scope, $location,  $timeo
         $mdSidenav('editMenu').close();
     };
 
-    function debounce(func, wait, context) {
-        var timer;
-
-        return function debounced() {
-            var context = $scope,
-                args = Array.prototype.slice.call(arguments);
-
-            $timeout.cancel(timer);
-            timer = $timeout(function() {
-                timer = undefined;
-                func.apply(context, args);
-            }, wait || 10);
-        };
-    }
-
     function buildToggler(navID) {
         return function() {
             $mdSidenav(navID).toggle();
--- app/common/infoMenu/infoMenuController.js
+++ app/common/infoMenu/infoMenuController.js
@@ -10,21 +10,6 @@ keijibanApp.controller('infoMenuController', function($scope, $location,  $timeo
       $mdSidenav('infoMenu').close();
     };
 
-    function debounce(func, wait, context) {
-        var timer;
-
-        return function debounced() {
-            var context = $scope,
-                args = Array.prototype.slice.call(arguments);
-
-            $timeout.cancel(timer);
-            timer = $timeout(function() {
-                timer = undefined;
-                func.apply(context, args);
-            }, wait || 10);
-        };
-    }
-
     function buildToggler(navID) {
         return function() {
             $mdSidenav(navID).toggle();
