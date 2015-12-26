2169e78fd2d5df1e04cec83e1081697526106724
Christopher Fisher <chris@alphafitch.uk>
2015-12-26 10:31:48 +0000

    Remove the info route from app.routes as it is no longer a separate view as per issue #12

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,10 +4,6 @@ keijibanApp.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'app/components/list/listView.html',
             controller: 'listController'
         })
-        .when('/info', {
-            templateUrl: 'app/components/info/infoView.html',
-            controller: 'infoController'
-        })
         .when('/noticeboard', {
             templateUrl: 'app/components/noticeboard/noticeboardView.html',
             controller: 'noticeboardController'
