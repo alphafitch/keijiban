9db9f241009d72fb1319ea3868bd695aef3759c3
Christopher Fisher <chris@alphafitch.uk>
2015-12-26 12:06:24 +0000

    Tidied up the editMenu controller, removed delayedToggler function which isn't used, re-indented the code for correct white spacing and simplified duplicated controller

--- app/common/editMenu/editMenuController.js
+++ app/common/editMenu/editMenuController.js
@@ -3,45 +3,32 @@ keijibanApp.controller('editMenuController', function($scope, $location,  $timeo
     $scope.toggleEditMenu = buildToggler('editMenu');
 
     $scope.isOpenRight = function(){
-      return $mdSidenav('editMenu').isOpen();
+        return $mdSidenav('editMenu').isOpen();
     };
-
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        $mdSidenav(navID).toggle();
-      }, 200);
-    }
-    function buildToggler(navID) {
-      return function() {
-        $mdSidenav(navID).toggle();
-      };
-    }
-
-    }).controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
 
     $scope.close = function () {
-      $mdSidenav('editMenu').close();
+        $mdSidenav('editMenu').close();
     };
+
+    function debounce(func, wait, context) {
+        var timer;
+
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+                timer = undefined;
+                func.apply(context, args);
+            }, wait || 10);
+        };
+    }
+
+    function buildToggler(navID) {
+        return function() {
+            $mdSidenav(navID).toggle();
+        };
+    }
 
 });
