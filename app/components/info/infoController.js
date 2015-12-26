f404469eb86b1280f49f0363551a6aa44e99461f
Christopher Fisher <chris@alphafitch.uk>
2015-12-26 10:22:32 +0000

    Remove the info component for #12 as everything is now in common/info

--- app/components/info/infoController.js
+++
@@ -1,9 +0,0 @@
-keijibanApp.controller('infoController', function($scope, $http, $location) {
-
-    $scope.buttonLabel = "list";
-
-    $http.get('app/components/info/config.json').then(function(response) {
-        $scope.info = response.data;
-    });
-
-});
