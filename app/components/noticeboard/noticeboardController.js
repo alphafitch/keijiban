0f1c6be7a3828c00ae713b14b6a7bb027509e05e
Christopher Fisher <chris@alphafitch.uk>
2015-12-28 22:27:25 +0000

    #3 Added new builder function in the noticeboard controller to create the text for the event module

--- app/components/noticeboard/noticeboardController.js
+++ app/components/noticeboard/noticeboardController.js
@@ -2,4 +2,18 @@ keijibanApp.controller('noticeboardController', function($scope, $rootScope) {
 
     $scope.currentBoard = $scope.boards[$rootScope.currentSelection];
 
+    // Modifies the default event text to fit the config for each noticeboard
+    $scope.buildEventModuleText = function() {
+        var defaultText = $scope.modules.event.text,
+            eventDate = $scope.currentBoard.event.date,
+            eventStart = $scope.currentBoard.event.startTime,
+            eventEnd = $scope.currentBoard.event.endTime;
+
+            defaultText = defaultText.replace("{date}", eventDate);
+            defaultText = defaultText.replace("{startTime}", eventStart);
+            defaultText = defaultText.replace("{endTime}", eventEnd);
+
+            return defaultText;
+    };
+
 });
