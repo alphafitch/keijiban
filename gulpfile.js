4ae644762a3e4aeeb4493ad5ac7d801931d94802
Matthias Bremser <matthias@bremser.eu>
2016-01-03 18:13:58 +0000

    add gulp-zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
+var zip = require('gulp-zip');
 
 // default gulp task
-gulp.task('default', function() {
+gulp.task('default', ['dist'], function() {
   
+});
+
+gulp.task('dist', function() {
+  return gulp.src('dist/*')
+    .pipe(zip('keijiban.zip'))
+    .pipe(gulp.dest('dist'));
 });
