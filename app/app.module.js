e11e8a13c88ca025e42b403ef941623cd97ef3ab
Christopher Fisher <chris@alphafitch.uk>
2016-01-07 14:48:59 +0000

    Adding a filter in the app modules for range, useful in ng-repeat loops

--- app/app.module.js
+++ app/app.module.js
@@ -35,3 +35,13 @@ keijibanApp.filter('titleCase', function() {
         });
     };
 });
+
+keijibanApp.filter('range', function() {
+    return function(input, total) {
+        total = parseInt(total);
+        for (var i=0; i<total; i++) {
+            input.push(i);
+        }
+        return input;
+    };
+});
