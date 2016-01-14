26642cb2148da0265365f17942f694e524855f0b
Mohamed Kherroubi <mkherroubi93@gmail.com>
2016-01-14 00:53:57 +0000

    Initial chunking functionality.
    Signed-off-by: Mohamed Kherroubi <mkherroubi93@gmail.com>

--- app/app.controller.js
+++ app/app.controller.js
@@ -11,6 +11,39 @@ keijibanApp.controller('appController', function($scope, $http, $location, $mdSi
         $scope.list = response.data.list;
         $scope.modules = response.data.boards.modules;
         $scope.boards = response.data.boards.data;
+        function baseSlice(array, start, end) {
+            var index = -1,
+                length = array.length;
+            if (start < 0) {
+                start = -start > length ? 0 : (length + start);
+            }
+            end = end > length ? length : end;
+            if (end < 0) {
+                end += length;
+            }
+            length = start > end ? 0 : ((end - start) >>> 0);
+            start >>>= 0;
+            var result = Array(length);
+            while (++index < length) {
+                result[index] = array[index + start];
+            }
+            return result;
+        }
+        function chunk(array, size) {
+            size = Math.max(size, 0);
+            var length = array ? array.length : 0;
+            if (!length || size < 1) {
+                return [];
+            }
+            var index = 0,
+                resIndex = -1,
+                result = Array(Math.ceil(length / size));
+            while (index < length) {
+                result[++resIndex] = baseSlice(array, index, (index += size));
+            }
+            return result;
+        };
+        $scope.chunks = chunk($scope.boards, $scope.list.maxWidth);
     });
 
     // On load return to the list page becuase the app is stateful
