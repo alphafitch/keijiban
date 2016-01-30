ca662bc1e54c740a161ed8d4ffc7462e1b9efe4a
Christopher Fisher <chris@alphafitch.uk>
2016-01-30 10:49:49 +0000

    Refactor for #2 - Attached baseSlice and chunk functions to the window object so that they can be used anywhere

--- app/app.controller.js
+++ app/app.controller.js
@@ -14,56 +14,50 @@ keijibanApp.controller('appController', function($scope, $http, $location, $mdSi
         $scope.modules = response.data.notices.modules;
         $scope.notices = response.data.notices.data;
 
-        // Calculates the size of the sub array in a 2D 'chunked' array
-        function baseSlice(array, start, end) {
-            var index = -1,
-                length = array.length,
-                result = 0;
-
-            if (start < 0) {
-                start = -start > length ? 0 : (length + start);
-            }
-
-            end = end > length ? length : end;
-
-            if (end < 0) {
-                end += length;
-            }
-
-            length = start > end ? 0 : ((end - start) >>> 0);
-            start >>>= 0;
-            result = Array(length);
-
-            while (++index < length) {
-                result[index] = array[index + start];
-            }
-
-            return result;
-        }
-
-        // A function that converts a normal array into a 2D array
-        function chunk(array, size) {
-            var length = array ? array.length : 0,
-                index = 0,
-                resIndex = -1,
-                result = Array(Math.ceil(length / size));
-
-            size = Math.max(size, 0);
-
-            if (!length || size < 1) {
-                return [];
-            }
-
-            while (index < length) {
-                result[++resIndex] = baseSlice(array, index, (index += size));
-            }
-
-            return result;
-        }
-
         // Chunk up the list of boards so that the grid can be displayed to match the config
         $scope.chunks = chunk($scope.notices, $scope.board.maxWidth);
     });
+
+    // Calculates the size of the sub array in a 2D 'chunked' array
+    window.baseSlice = function(array, start, end) {
+        var index = -1,
+            length = array.length,
+            result = 0;
+
+        if (start < 0) {
+            start = -start > length ? 0 : (length + start);
+        }
+        end = end > length ? length : end;
+        if (end < 0) {
+            end += length;
+        }
+        length = start > end ? 0 : ((end - start) >>> 0);
+        start >>>= 0;
+        result = Array(length);
+        while (++index < length) {
+            result[index] = array[index + start];
+        }
+
+        return result;
+    };
+
+    // Converts a normal array into a 2D array
+    window.chunk = function(array, size) {
+        var length = array ? array.length : 0,
+            index = 0,
+            resIndex = -1,
+            result = Array(Math.ceil(length / size));
+
+        size = Math.max(size, 0);
+        if (!length || size < 1) {
+            return [];
+        }
+        while (index < length) {
+            result[++resIndex] = baseSlice(array, index, (index += size));
+        }
+
+        return result;
+    };
 
     // On load return to the board page becuase the app is stateful
     window.onload = function() {
