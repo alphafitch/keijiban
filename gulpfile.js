4ef6b545c1d9458e97c84939047fc5d6696bf05d
Christopher Fisher <chris@alphafitch.uk>
2016-01-31 10:22:07 +0000

    #23 Concatenating and uglifying all JS files before zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
-var gulp = require('gulp');
-var zip = require('gulp-zip');
-var minify = require('gulp-minify');
+var gulp = require('gulp'),
+    zip = require('gulp-zip'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 
 // default gulp task
 gulp.task('default', ['zip'], function() {
 
 });
 
-gulp.task('zip', ['minify'], function() {
+gulp.task('zip', ['uglify'], function() {
   return gulp.src('dist/*')
     .pipe(zip('keijiban.zip'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify', function() {
-  return gulp.src('app/*.js')
-    .pipe(minify({
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js']
-    }))
+gulp.task('uglify', function() {
+  return gulp.src('app/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
