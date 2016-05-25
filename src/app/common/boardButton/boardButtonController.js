249f1ec403bd7e3f3e5befe54d3b2303f8a4d3f9
Christopher Fisher <chris.d.fisher@directlinegroup.co.uk>
2016-05-25 15:08:43 +0100

    Fixing issue #6 by scrolling the window to the top on going back to the board

--- src/app/common/boardButton/boardButtonController.js
+++ src/app/common/boardButton/boardButtonController.js
@@ -1,6 +1,7 @@
-keijiban.controller("boardButtonController", function($scope, $location) {
+keijiban.controller("boardButtonController", function($scope, $location, $window) {
 
     $scope.goToList = function() {
+        $window.scrollTo(0, 0); // Jump to the top of the page
         $location.url("board");
     };
 
